bd26697ec81dac445935370ab036b568a7d5665f
martin-dzhonov <martin.dzhonov@gmail.com>
2014-10-10 01:04:12 +0300

    user accounts

---
+++ src/com/gerfield/places/authentication/UdinicAuthenticatorService.java
@@ -0,0 +1,21 @@
+package com.gerfield.places.authentication;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Udini
+ * Date: 19/03/13
+ * Time: 19:10
+ */
+public class UdinicAuthenticatorService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        AbstractAccountAuthenticator authenticator = new UdinicAuthenticator(this);
+        return authenticator.getIBinder();
+    }
+}
